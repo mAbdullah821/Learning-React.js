237be55cfdca0f1fa48363b6e909f46ba64013f7
Mahmoud821 <mahmoud.zoro821@gmail.com>
2023-03-04 14:27:31 +0200

    Add more conditional rendering
    		- Create them using {&&, condition ? action_for_true : action_for_false}
    		- Add a 'key' attribute to uniqely identify the
    		  list_element
    		- Use 'shortid' library to generate unique keys
    		- Create a button to toggle the state of the variable
    		  'isAuth'

--- src/users/users.js
+++ src/users/users.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import shortid from "shortid";
 
 export default class Users extends React.Component {
   constructor() {
     super();
     this.state = {
+      isAuth: true,
       users: [
         { name: "me", age: 12, display: true },
         { name: "do not display", age: 13, display: false },
@@ -14,28 +17,59 @@ export default class Users extends React.Component {
     };
   }
 
+  toggleIsAuth() {
+    this.setState({ isAuth: !this.state.isAuth });
+  }
+
   render() {
     // Conditional Rendering
+    // Only use 'Array.map()' to create an element list.
+    // As a rule of thumb, we should add a 'key' attribute for each element when we use 'Array.map()' to uniquely identify each element for good rendering performance, don't use the element 'index' as a unique key, the key must be unique at the level of the same type siblings element, e.g
+    // (
+    //   <ul>
+    //     <li key="unique_key"> text </li>
+    //     <li key="unique_key"> text </li>
+    //     <li key="unique_key"> text </li>
+    //   </ul>
+    // )
+
     return (
       <>
         {/* First example */}
-        {this.state.users.map((user) => {
-          if (user.display) {
-            return <button> Hello, {user.name}</button>;
-          }
-        })}
-        {/* Second example */}
-        <ul>
-          {this.state.users.map((user) => {
+        {this.state.isAuth &&
+          this.state.users.map((user) => {
             if (user.display) {
               return (
-                <li>
-                  Welcome, {user.name} with {user.age} years old.
-                </li>
+                <button key={shortid.generate()}> Hello, {user.name}</button>
               );
             }
           })}
+        {/* Second example */}
+        <ul>
+          {this.state.isAuth ? (
+            this.state.users.map((user) => {
+              if (user.display) {
+                return (
+                  <li key={shortid.generate()}>
+                    Welcome, {user.name} with {user.age} years old.
+                  </li>
+                );
+              }
+            })
+          ) : (
+            <h1> Please Login </h1>
+          )}
         </ul>
+        {/* Separator Comment */}
+        <div className="d-grid gap-2">
+          <Button
+            variant="success"
+            size="lg"
+            onClick={() => this.toggleIsAuth()}
+          >
+            Toggle isAuth
+          </Button>
+        </div>
       </>
     );
   }
